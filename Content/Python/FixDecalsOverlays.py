f69734c1075d413127e9f017e3d1a2c85ec8fc33
Alexei Gladkikh <gladkikhalexei@gmail.com>
2023-10-23 23:18:12 +0300

    #5 Added material recompile

--- Content/Python/FixDecalsOverlays.py
+++ Content/Python/FixDecalsOverlays.py
@@ -2,6 +2,8 @@
 
 
 def fix_decals_overlays():
+    materials = list()
+
     for actor in unreal.EditorLevelLibrary.get_all_level_actors():
         name: str = actor.get_name()
         if not name.startswith("info_overlay"):
@@ -16,10 +18,10 @@ def fix_decals_overlays():
         for index in range(mesh_component.get_num_materials()):
             material = mesh_component.get_material(index)
 
-            material.set_editor_property("blend_mode", unreal.BlendMode.BLEND_TRANSLUCENT)
-
             assert isinstance(material, unreal.Material), \
                 f"Material required, not a material interface for {material.get_name()}"
+
+            material.set_editor_property("blend_mode", unreal.BlendMode.BLEND_TRANSLUCENT)
 
             texture = unreal.MaterialEditingLibrary.get_material_property_input_node(
                 material, unreal.MaterialProperty.MP_BASE_COLOR)
@@ -27,9 +29,12 @@ def fix_decals_overlays():
             status = unreal.MaterialEditingLibrary.connect_material_property(
                 texture, "A", unreal.MaterialProperty.MP_OPACITY)
 
-            unreal.EditorAssetLibrary.save_loaded_asset(material)
+            unreal.MaterialEditingLibrary.recompile_material(material)
 
             print(f" -> Add alpha channel to material: '{material.get_name()}' status={status}")
+            materials.append(material)
+
+    unreal.EditorAssetLibrary.save_loaded_assets(materials, only_if_is_dirty=False)
 
 
 if __name__ == '__main__':
