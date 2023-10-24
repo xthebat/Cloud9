34db7a751d6dab53dcbaa9519e1a5b472d39ae17
Alexei Gladkikh <gladkikhalexei@gmail.com>
2023-10-24 19:31:00 +0300

    #12 Fixed PostProcessingVolume properties

--- Content/Python/Map/FixEnvironmentLight.py
+++ Content/Python/Map/FixEnvironmentLight.py
@@ -5,9 +5,10 @@ import unreal
 
 def fix_environment_light(
         location: Tuple[float, float, float] = (0.0, 0.0, 0.0),
-        shift: Tuple[float, float, float] = (0.0, 100.0, 0.0),
+        shift: Tuple[float, float, float] = (-200.0, 0.0, 0.0),
 
         sun_brightness: float = 75.0,
+        sun_height: float = 0.736771,
         cloud_speed: float = 2.0,
         cloud_opacity: float = 1.0,
 
@@ -34,15 +35,17 @@ def fix_environment_light(
     bp_sky_sphere = unreal.EditorAssetLibrary.load_blueprint_class("/Engine/EngineSky/BP_Sky_Sphere")
     actor = unreal.EditorLevelLibrary.spawn_actor_from_class(bp_sky_sphere, location)
     actor.set_folder_path(folder)
+    actor.set_actor_label("SkySphere")
     actor.set_editor_property("Sun Brightness", sun_brightness)
     actor.set_editor_property("Cloud Speed", cloud_speed)
     actor.set_editor_property("Cloud Opacity", cloud_opacity)
+    actor.set_editor_property("Cloud Opacity", cloud_opacity)
+    actor.set_editor_property("Sun Height", sun_height)
     location += shift
 
     actor = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.DirectionalLight, location)
     actor = cast(unreal.DirectionalLight, actor)
     actor.set_folder_path(folder)
-    actor.set_actor_label("Light Source")
     actor.set_actor_rotation(light_direction, True)
     actor.light_component.set_intensity(light_intensity)
     actor.light_component.set_atmosphere_sun_light(True)
@@ -52,6 +55,13 @@ def fix_environment_light(
     actor = cast(unreal.PostProcessVolume, actor)
     actor.set_folder_path(folder)
     actor.unbound = True
+    settings = actor.settings
+    settings.override_auto_exposure_bias = True
+    settings.override_auto_exposure_min_brightness = True
+    settings.override_auto_exposure_max_brightness = True
+    settings.auto_exposure_bias = 0.0
+    settings.auto_exposure_min_brightness = 1.0
+    settings.auto_exposure_max_brightness = 1.0
     location += shift
 
     actor = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.SkyLight, location)
