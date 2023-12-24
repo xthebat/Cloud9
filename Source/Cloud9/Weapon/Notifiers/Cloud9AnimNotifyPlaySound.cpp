0d05b8ce96aff2a704fa5fdf7ee6880d17c2ec07
Alexei Gladkikh <gladkikhalexei@gmail.com>
2023-12-24 19:01:46 +0300

    #77 WIP

---
+++ Source/Cloud9/Weapon/Notifiers/Cloud9AnimNotifyPlaySound.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2023 Alexei Gladkikh
+
+
+#include "Cloud9AnimNotifyPlaySound.h"
+
+#include "Cloud9/Tools/Macro/Common.h"
+#include "Cloud9/Game/Cloud9DeveloperSettings.h"
+#include "Cloud9/Character/Cloud9Character.h"
+#include "Cloud9/Weapon/Classes/Cloud9WeaponBase.h"
+
+void UCloud9AnimNotifyPlaySound::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	static let Settings = UCloud9DeveloperSettings::Get();
+
+	let Character = MeshComp->GetOwner<ACloud9Character>();
+
+	if (not IsValid(Character))
+	{
+		return;
+	}
+
+	let Weapon = Character->GetInventory()->GetSelectedWeapon();
+
+	if (not IsValid(Weapon))
+	{
+		return;
+	}
+
+	if (not IsValid(Weapon->WeaponDefinition))
+	{
+		return;
+	}
+
+	// TODO: MAKE IT USING MAP
+	
+	let Info = Weapon->WeaponDefinition.GetWeaponInfo<FFirearmWeaponInfo>();
+
+	if (Index >= Info->Sounds.ReloadSounds.Num())
+	{
+		return;
+	}
+
+	Weapon->PlaySound(Info->Sounds.ReloadSounds[Index], Settings->Volume);
+}
